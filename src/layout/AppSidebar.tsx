e2270076154691926a75c3bbbdc820a00407c829
darshan <darshan@itfuturz.com>
2025-09-12 18:10:02 +0530

    whatsapp module

--- src/layout/AppSidebar.tsx
+++ src/layout/AppSidebar.tsx
@@ -304,9 +304,9 @@ const handleLogout = () => {
       className={`fixed mt-16 flex flex-col lg:mt-0 top-0 px-5 left-0 bg-white dark:bg-gray-900 dark:border-gray-800 text-gray-900 h-screen transition-all duration-300 ease-in-out z-50 border-r border-gray-200 
         ${
           isExpanded || isMobileOpen
-            ? "w-[290px]"
+            ? "w-[230px]"
             : isHovered
-            ? "w-[290px]"
+            ? "w-[230px]"
             : "w-[90px]"
         }
         ${isMobileOpen ? "translate-x-0" : "-translate-x-full"}
